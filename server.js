baae064d645150ea34a3841c5eba17b9d929051c
lscambo13 <ls.cambo13@gmail.com>
2023-04-23 00:03:48 +0530

    First Upload

---
+++ server.js
@@ -0,0 +1,45 @@
+// Import Express
+const express = require('express')
+
+// Create an instance of Express
+const app = express()
+
+// Define a port number
+const port = 8080
+
+const cors = require('cors')
+app.use(cors())
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function parseVideoLink(url) {
+  //console.log('stdout:', stdout);
+  //console.log('stderr:', stderr);
+  let stdout, stderr;
+  try {
+    res = await exec(`yt-dlp -g ${url} -f best*[vcodec!=none][acodec!=none]`);
+  console.log("sdf",res.stdout, res.stderr)
+
+  }
+  catch (err) {
+    console.log(err)
+    return
+  }
+  return { url : res.stdout};
+}
+
+
+// Create a route handler for the root path
+app.get('/',async (req, res) => {
+  //console.log(req.query)
+  const query = req.query.url
+  const decodedURL = await parseVideoLink(query)
+  console.log(decodedURL)
+  res.send(decodedURL);
+})
+
+// Start the server
+app.listen(port, () => {
+  console.log('Server is running on port ' + port)
+})
